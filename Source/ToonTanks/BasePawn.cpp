7f31c9dae45cb012651afc4793b6cec4e2a99a51
jaejune Yun <jjune0427@naver.com>
2022-11-17 22:44:55 +0900

    Tank RootComponent 설정

--- Source/ToonTanks/BasePawn.cpp
+++ Source/ToonTanks/BasePawn.cpp
@@ -2,6 +2,7 @@
 
 
 #include "BasePawn.h"
+#include "Components/CapsuleComponent.h"
 
 // Sets default values
 ABasePawn::ABasePawn()
@@ -9,6 +10,8 @@ ABasePawn::ABasePawn()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	//RootComponent;
+	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Collider"));
+	RootComponent = CapsuleComp;		//CapsuleComp를 RootComponent로 명시적으로 지정
 
 }
 
@@ -16,6 +19,8 @@ ABasePawn::ABasePawn()
 void ABasePawn::BeginPlay()
 {
 	Super::BeginPlay();
+	//구성요소 또는 Subobject(하위객체)를 만들고 만든 것을 Default로 변환 return type은 <>안에 있는 타입. TEXT는 이름을 정한 것이다.
+	
 	
 }
 
